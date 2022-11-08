ce09aaf75b4bcdb2da386f29570f94abb8df11f2
Ronald Pinheiro <109386027+pinheiroronald@users.noreply.github.com>
2022-11-08 20:31:00 -0300

    atu

---
+++ banco_açai/codg.js
@@ -0,0 +1,15 @@
+let nome =[]
+let conta =[]
+let senha =[]
+let nom = window.document.querySelector('input#nome')
+let con = window.document.querySelector('input#conta')
+let sen = window.document.querySelector('input#senha')
+
+
+ function cadastro(){ 
+
+    
+  if(nom =='' || con =='' || sen == '')
+  window.alert('ok')
+
+ }
